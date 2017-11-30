c830170a336bedc766081a5502240b53f5e4ca8a
yzjdz <1045833302@qq.com>
2017-11-30 10:19:39 +0800

    Delete start.sh

--- models/googlenet/操作/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-LOG=/home/nssg/unclezhannssg/Downloads/anzhuangcaffe/caffe-master/examples/myfilen/res/googlenet/LOG/-`date +%Y-%m-%d-%H-%M-%S`.log
-/home/nssg/unclezhannssg/Downloads/anzhuangcaffe/caffe-master/build/tools/caffe train -solver /home/nssg/unclezhannssg/Downloads/anzhuangcaffe/caffe-master/examples/myfilen/res/googlenet/solver.prototxt -gpu 0 	2>&1   | tee $LOG
