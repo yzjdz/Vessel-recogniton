5334603bce65554a2c07c0c60b74e4ebb4d7770a
yzjdz <1045833302@qq.com>
2017-11-30 10:15:53 +0800

    Delete goon.sh

--- models/Densenet13/操作/goon.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh  
-LOG=/home/nssg/unclezhannssg/Downloads/anzhuangcaffe/caffe-master/examples/myfilen/DenseNet13/LOG/-`date +%Y-%m-%d-%H-%M-%S`.log
-/home/nssg/unclezhannssg/Downloads/anzhuangcaffe/caffe-master/build/tools/caffe train -solver /home/nssg/unclezhannssg/Downloads/anzhuangcaffe/caffe-master/examples/myfilen/DenseNet13/solver.prototxt -snapshot /home/nssg/unclezhannssg/Downloads/anzhuangcaffe/caffe-master/examples/myfilen/DenseNet13/LOG_iter_280000.solverstate -gpu 2 	2>&1   | tee $LOG
